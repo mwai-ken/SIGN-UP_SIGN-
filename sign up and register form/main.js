2de05bfa8f7ca40590c86e514fae7c90b0e88d81
mwai-ken <97677013+mwai-ken@users.noreply.github.com>
2022-04-07 08:22:31 +0300

    Add files via upload

---
+++ sign up and register form/main.js
@@ -0,0 +1,14 @@
+
+
+var card = document.getElementById("card");
+
+function openRegister(){
+    card.style.transform="rotateY(-180deg)";
+
+}
+function openlogin(){
+    card.style.transform="rotateY(0deg)";
+    
+}
+
+
